4a66c21ceea5dafdf0dc482629c3c1a322afdc49
Jobber Great <jobbergreatbro@gmail.com>
2023-03-28 02:39:23 +0000

    adicionando função de subtrair, dividir e mul. com seus respectivos botões

--- App.js
+++ App.js
@@ -2,11 +2,62 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  const [nome, setNome] = useState("Maria");
+  /*usando hook state para armazenar nome para ser exibido*/
+  const [nome, setNome] = useState("");
+  /*usando hook state para armazenar nome digitado e linkado com TextInput*/
+  const [nomeTxt, setNomeTxt] = useState("");
 	
-  function lerNome(){
-	  setNome("Ana");
+  /*usando hook state para armazenar o valor e linkado com TextInput para o valor1*/
+  const [valor1, setValor1] = useState(0);
+  /*usando hook state para armazenar o valor e linkado com TextInput para o valor2*/
+  const [valor2, setValor2] = useState(0);
+  /*usando hook state para armazenar o valor do resultado da operação e que será exibido*/
+  const [resultado, setResultado] = useState(0);
+  
+  /*Função para realizar o cálculo da soma de valor1 e valor2*/
+  function somar(){
+	  //calculando o valor da soma e atribuindo o resultado para a variável r
+	  let r = parseFloat(valor1) + parseFloat(valor2);
+	  //alterando o valor do resultado inserindo o valor da variável r
+	  setResultado(r);
   }
+  
+  /*Função para realizar o cálculo da subtração de valor1 e valor2*/
+  function subtrair(){
+	  //calculando o valor da subtração e atribuindo o resultado para a variável r
+	  let r = parseFloat(valor1) - parseFloat(valor2);
+	  //alterando o valor do resultado inserindo o valor da variável r
+	  setResultado(r);
+  }
+  
+  /*Função para realizar o cálculo da multiplicação de valor1 e valor2*/
+  function multiplicar(){
+	  //calculando o valor da multiplicação e atribuindo o resultado para a variável r
+	  let r = parseFloat(valor1) * parseFloat(valor2);
+	  //alterando o valor do resultado inserindo o valor da variável r
+	  setResultado(r);
+  }
+  
+  /*Função para realizar o cálculo da divisão de valor1 e valor2*/
+  function dividir(){
+	  //verificando se o valor2 é diferente de zero
+	  if(parseFloat(valor2) !== 0){
+		  //calculando o valor da divisão e atribuindo o resultado para a variável r
+		  let r = parseFloat(valor1) / parseFloat(valor2);
+		  //alterando o valor do resultado inserindo o valor da variável r
+		  setResultado(r);
+	  }else{
+		  //alterando o valor do resultado para "Erro" caso valor2 seja zero
+		  setResultado("Erro");
+	  }
+  }
+
+  //função que altera o valor da state nome
+  function lerNome(){
+	  //atribuindo valor da state nomeTxt para a state nome
+	  setNome(nomeTxt);
+  }
+  
   return (
     <View style={styles.container}>
       
@@ -14,16 +65,61 @@ export default function App() {
 
 		  <View style={styles.bloco}>
 			  <Text style={styles.label}> Nome: </Text>
+
+			   {/*input do nome*/}
 			  <TextInput 
 				  style={styles.input}
-				  value={nome}
-				  onChangeText={(texto)=>setNome(texto)}
+				  value={nomeTxt}
+				  onChangeText={(texto)=>setNomeTxt(texto)}
 			  />
 			  <TouchableOpacity style={styles.botao} onPress={lerNome}>
 				  <Text style={styles.txtBotao}>Enviar</Text>
 			  </TouchableOpacity>
-			  
+			   {/*exibindo o nome*/}
 			  <Text style={styles.titulo}>Nome: {nome}</Text>
+		  </View>
+		
+		  <View style={styles.bloco}>
+		  	<Text style={styles.label}> Valor 1: </Text>
+			    {/*input do valor1*/}
+			  <TextInput 
+				  style={styles.input}
+				  value={valor1}
+				  onChangeText={(valor)=>setValor1(valor)}
+			  />
+		  </View>
+		  <View style={styles.bloco}>
+		  	<Text style={styles.label}> Valor 2: </Text>
+			  {/*input do valor1*/}
+			  <TextInput 
+				  style={styles.input}
+				  value={valor2}
+				  onChangeText={(valor)=>setValor2(valor)}
+			  />
+		  </View>
+		  <View style={styles.bloco}> 
+		  	<TouchableOpacity style={styles.botao} onPress={somar}>
+				  <Text style={styles.txtBotao}>Somar</Text>
+			  </TouchableOpacity>
+		  </View>
+		    <View style={styles.bloco}> 
+		  	<TouchableOpacity style={styles.botao} onPress={subtrair}>
+				  <Text style={styles.txtBotao}>Subtrair</Text>
+			  </TouchableOpacity>
+		  </View>
+		  <View style={styles.bloco}> 
+		  	<TouchableOpacity style={styles.botao} onPress={multiplicar}>
+				  <Text style={styles.txtBotao}>Multiplicar</Text>
+			  </TouchableOpacity>
+		  </View>
+		  <View style={styles.bloco}> 
+		  	<TouchableOpacity style={styles.botao} onPress={dividir}>
+				  <Text style={styles.txtBotao}>Dividir</Text>
+			  </TouchableOpacity>
+		  </View>
+		  <View style={styles.bloco}>
+			  {/*exibindo o valor do resultado*/}
+		  	<Text style={styles.label}> Resultado: {resultado}</Text>
 		  </View>
     </View>
 	
